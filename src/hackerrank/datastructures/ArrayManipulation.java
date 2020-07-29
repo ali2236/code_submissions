aa3182fbdf8e6d6b8e44a5f501a1dc50db8b6a05
Aligator <aligator2236@gmail.com>
2020-07-29 14:45:28 +0430

    solved a few DS problems

---
+++ src/hackerrank/datastructures/ArrayManipulation.java
@@ -0,0 +1,64 @@
+package hackerrank.datastructures;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ArrayManipulation {
+
+    // Complete the arrayManipulation function below.
+    static long arrayManipulation(int n, int[][] queries) {
+        long[] a = new long[n];
+
+        for (int[] query : queries) {
+            int s = query[0]-1;
+            int e = query[1];
+            long amount = query[2];
+
+            a[s] += amount;
+            a[e] -= amount;
+        }
+
+        long sum = 0;
+        long max = 0;
+        for (long l : a) {
+            sum += l;
+            if(sum>max) max = sum;
+        }
+        return max;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        int[][] queries = new int[m][3];
+
+        for (int i = 0; i < m; i++) {
+            String[] queriesRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 3; j++) {
+                int queriesItem = Integer.parseInt(queriesRowItems[j]);
+                queries[i][j] = queriesItem;
+            }
+        }
+
+        long result = arrayManipulation(n, queries);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
