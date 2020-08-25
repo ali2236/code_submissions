75e86e7dbdd8e75c5e781d7d9ae64d3894007b9b
Aligator <aligator2236@gmail.com>
2020-08-25 21:17:03 +0430

    bad string

--- src/codeforces/AGoodString.java
+++ src/codeforces/AGoodString.java
@@ -14,30 +14,26 @@ public class AGoodString {
     }
 
     private static int minimumChanges(char[] str, int l, int r, char c) {
-        if ((r - l + 1) == 1) {
-            return c == str[l] ? 0 : 1;
-        }
+        if (r - l == 0) return c == str[l] ? 0 : 1;
 
         int mid = (r + l) / 2;
 
-        int d = (r - l + 1) / 2;
-
         // first half
-        int changesLeft = minimumChanges(str, l, mid, (char) (c + 1)) + (d - count(str, mid+1, r, c));
+        int changesLeft = changeCount(str, mid + 1, r, c) + minimumChanges(str, l, mid, (char) (c + 1));
 
         // second half
-        int changesRight = minimumChanges(str, mid+1, r, (char) (c + 1)) + (d - count(str, l, mid, c));
+        int changesRight = changeCount(str, l, mid, c) + minimumChanges(str, mid + 1, r, (char) (c + 1));
 
         return Math.min(changesLeft, changesRight);
     }
 
-    private static int count(char[] str, int from, int to, int c) {
+    private static int changeCount(char[] str, int from, int to, int c) {
 
         int count = 0;
         for (int i = from; i <= to; i++) {
             char ch = str[i];
             if (ch == c) count++;
         }
-        return count;
+        return (to - from + 1) - count;
     }
 }
