71c502dd51b4cfe082502c21bd032309efd4e5d4
Aligator <aligator2236@gmail.com>
2020-10-03 20:31:01 +0330

    ...

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Solution {
+
+    static int[] color;
+    static List<Integer>[] adj;
+    static int[] dp;
+
+    public static void main(String[] args) {
+        FastReader scanner = new FastReader();
+
+        // input
+        int n = scanner.nextInt();
+
+        color = new int[n+1];
+        for (int i = 1; i <= n; i++) color[i] = scanner.nextInt();
+
+        adj = new List[n + 1];
+        for (int i = 1; i <= n; i++) {
+
+        }
+
+        for (int i = 0; i < n; i++) {
+            int parent = scanner.nextInt();
+            int node = scanner.nextInt();
+            adj[parent].add(node);
+        }
+        // calculate
+
+        // print
+    }
+
+    static class FastReader {
+
+        private StringTokenizer st;
+        private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        public String next() {
+            while(st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(nextLine());
+            }
+            return st.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public String nextLine() {
+            try {
+                return br.readLine();
+            } catch(IOException ex) {
+                ex.printStackTrace();
+                return null;
+            }
+        }
+
+    }
 
 }
 
--- src/codeforces/c1324/SleepingSchedule.java
+++ src/codeforces/c1324/SleepingSchedule.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SleepingSchedule {
 
     static int n, h, l, r;
+    static int[] a;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -12,35 +13,29 @@ public class SleepingSchedule {
         h = scanner.nextInt();
         l = scanner.nextInt();
         r = scanner.nextInt();
-        int[] a = new int[n];
+        a = new int[n];
         for (int i = 0; i < n; i++) a[i] = scanner.nextInt();
         dp = new int[n + 1][h + 1];
         for (int i = 0; i < n + 1; i++)
             for (int j = 0; j < h + 1; j++) dp[i][j] = -1;
-        System.out.println(solve(a, 0, 0));
+        System.out.println(solve(0, 0));
     }
 
     static int[][] dp;
-
-    private static int solve(int[] a, int s, int t) {
+    private static int solve(int s, int t) {
         // is time good
-        int goodTime = 0;
-        if (s != 0 && t >= l && t <= r) {
-            goodTime = 1;
-        }
+        int goodTime = s != 0 && t >= l && t <= r ? 1 : 0;
 
         if (s == a.length) return goodTime;
 
-        if (dp[s][t] != -1) {
-            return dp[s][t];
-        }
+        if (dp[s][t] != -1) return dp[s][t];
 
         // stay awake
         int awake = a[s];
 
         // go to sleep
-        int a0 = dp[s + 1][(t + awake) % h] = solve(a, s + 1, (t + awake) % h);
-        int a1 = dp[s + 1][(t + awake - 1) % h] = solve(a, s + 1, (t + awake - 1) % h);
+        int a0 = dp[s + 1][(t + awake) % h] = solve(s + 1, (t + awake) % h);
+        int a1 = dp[s + 1][(t + awake - 1) % h] = solve(s + 1, (t + awake - 1) % h);
 
         return Math.max(a0, a1) + goodTime;
     }
